49cc83da6f8d363ebec5b6f1ca785349911c03e3
Matheo <matheoledesma1@gmail.com>
2020-07-03 00:27:57 -0300

    obelisco

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -6,28 +6,3 @@ $(window).scroll( () => {
 
     }
 })
-const db = firebase.firestore();
-
-const clientForm = document.getElementById('client-form');
-
-const saveClient = (name,email,message) =>
-    db.collection('stelhax-clientes').doc().set({
-     name,
-     email,
-	 message
-});
-
-
-
-clientForm.addEventListener('submit', async (e) =>{
-    e.preventDefault();
-
-    const name = clientForm['client-name'];
-    const email = clientForm['client-email'];
-	const message = clientForm['client-message'];
-
-    await saveClient(name.value,email.value,message.value);
-
-    clientForm.reset();
-
-});
